5b21f24da996425b75faca019975e26ffd4d91bb
yruefenacht <yannick.ruefenacht@hotmail.com>
2019-12-12 16:19:49 +0100

    day 12 part 2 solution

--- day12.py
+++ day12.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import math
+
 class Moon:
     def __init__(self, x, y, z):
         self.x = x
@@ -24,28 +26,23 @@ def print_moons(moons):
         print("x="+str(m.x)+" y="+str(m.y)+" z="+str(m.z)+"; ", end="")
         print("vx="+str(m.vx)+" vy="+str(m.vy)+" vz="+str(m.vz))
 
-def solve():
-    moon1 = Moon(3, 3, 0)
-    moon2 = Moon(4, -16, 2)
-    moon3 = Moon(-10, -6, 5)
-    moon4 = Moon(-3, 0, -13)
-    moons = (moon1, moon2, moon3, moon4)
-    steps = 1000
+def apply_gravity(m1, m2):
+    if m1.x > m2.x: m1.vx -= 1
+    elif m1.x < m2.x: m1.vx += 1
 
+    if m1.y > m2.y: m1.vy -= 1
+    elif m1.y < m2.y: m1.vy += 1
+
+    if m1.z > m2.z: m1.vz -= 1
+    elif m1.z < m2.z: m1.vz += 1
+
+def solve1(moons, steps):
     for step in range(steps):
         #1. Apply gravity to velocity
         for moon in moons:
             for m in moons:
                 if m is moon: continue
-
-                if moon.x > m.x: moon.vx -= 1
-                elif moon.x < m.x: moon.vx += 1
-
-                if moon.y > m.y: moon.vy -= 1
-                elif moon.y < m.y: moon.vy += 1
-
-                if moon.z > m.z: moon.vz -= 1
-                elif moon.z < m.z: moon.vz += 1
+                apply_gravity(moon, m)
         #2. Apply velocity to position
         for moon in moons:
             moon.apply_velocity()
@@ -53,5 +50,58 @@ def solve():
     part_one = sum(moon.get_energy() for moon in moons)
     print(part_one)
 
+def lcm(a, b):
+    return a * b // math.gcd(a, b)
+
+def solve2(moons):
+    steps = 0
+    x_found = y_found = z_found = None
+    x_history = y_history = z_history = set()
+
+    while True:
+        if x_found and y_found and z_found:
+            break
+
+        #1. Apply gravity to velocity
+        for moon in moons:
+            for m in moons:
+                if m is moon: continue
+                apply_gravity(moon, m)
+        #2. Apply velocity to position
+        for moon in moons:
+            moon.apply_velocity()
+
+        x = y = z = ""
+        for moon in moons:
+            x += str(moon.x) + str(moon.vx)
+            y += str(moon.y) + str(moon.vy)
+            z += str(moon.z) + str(moon.vz)
+        
+        if not x_found:
+            if x in x_history:
+                x_found = steps
+            else:
+                x_history.add(x)
+        if not y_found:
+            if y in y_history:
+                y_found = steps
+            else:
+                y_history.add(y)
+        if not z_found:
+            if z in z_history:
+                z_found = steps
+            else:
+                z_history.add(z)
+
+        steps += 1
+
+    part_two = lcm(lcm(x_found, y_found), z_found)
+    print(part_two)
+
 if __name__ == "__main__":
-    solve()
+    moon1 = Moon(3, 3, 0)
+    moon2 = Moon(4, -16, 2)
+    moon3 = Moon(-10, -6, 5)
+    moon4 = Moon(-3, 0, -13)
+    solve1((moon1, moon2, moon3, moon4), 1000)
+    solve2((moon1, moon2, moon3, moon4))
